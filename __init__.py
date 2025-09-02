6c12cad78a5652adf090eec832367ebdfd769ddd
Thieu Chuong Tat <chuongtat@gmail.com>
2025-09-02 10:55:46 -0400

    Add shortcut

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from aqt import mw
-from aqt.qt import QAction
+from aqt.qt import QAction, QKeySequence
 from .gui import AddNoteDialog
 
 def init():
     action = QAction("Auto-create vocab by Gemini", mw)
+    action.setShortcut(QKeySequence("Ctrl+G"))
     action.triggered.connect(lambda: AddNoteDialog(mw).exec())
     mw.form.menuTools.addAction(action)
 
